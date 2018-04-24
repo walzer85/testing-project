99e86a34e02d38b7ce018afe5e68bf805694b421
walzer85 <walzer85@gmail.com>
2018-04-24 08:25:28 -0500

    forgot to commit last changes last night.  test for map is maybe incomplete?

---
+++ map.test.js
@@ -0,0 +1,13 @@
+let dash = require('ewalz-dash');
+let filter = dash.filter;
+
+const names = ["Jon","Bob","Ted","Barney","Lilly","Robin","Saul","Axe"];
+const addHello = (names) => {
+    for (i = 0; i <= names.length; i++) {
+        return {'Hello' + names}
+    }
+}
+
+test('map returns a new array that says Hello array value', () => {
+    expect(helloArray).toEqual(["Hello Jon","Hello Bob","Hello Ted","Hello Barney","Hello Lilly","Hello Robin","Hello Saul","Hello Axe"]);
+});
