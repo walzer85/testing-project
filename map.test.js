d2c9950c6e99dec60de15301d638c1ffa8fdd75e
walzer85 <walzer85@gmail.com>
2018-04-24 19:16:56 -0500

    need to add the new array and insert the output into it.

--- map.test.js
+++ map.test.js
@@ -4,10 +4,12 @@ let filter = dash.filter;
 const names = ["Jon","Bob","Ted","Barney","Lilly","Robin","Saul","Axe"];
 const addHello = (names) => {
     for (i = 0; i <= names.length; i++) {
-        return {'Hello' + names}
+        return (`Hello {names[i]}`)
     }
 }
 
+let helloArray = addHello(names)
+
 test('map returns a new array that says Hello array value', () => {
     expect(helloArray).toEqual(["Hello Jon","Hello Bob","Hello Ted","Hello Barney","Hello Lilly","Hello Robin","Hello Saul","Hello Axe"]);
-});
+}); 
